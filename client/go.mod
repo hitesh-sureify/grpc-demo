cbe4e21a172f58c80f372ecb437f785ea8e76721
hitesh-sureify <hitesh.pattanayak@sureify.com>
2021-03-28 23:04:16 +0530

    added request fail prom ctrs

--- client/go.mod
+++ client/go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/hitesh-sureify/grpc-demo v0.0.0-20210328170500-7e30395c8448 // indirect
+	github.com/hitesh-sureify/grpc-demo v0.0.0-20210328171241-7474993f14f2 // indirect
 	github.com/prometheus/client_golang v1.10.0 // indirect
 	google.golang.org/grpc v1.36.1
 )
--- middleware/prometheus.go
+++ middleware/prometheus.go
@@ -9,16 +9,48 @@ import (
 )
 
 var (
-	counter = prometheus.NewCounter(
+	incoming_api_req_counter = prometheus.NewCounter(
 	   prometheus.CounterOpts{
 		  Namespace: "golang",
 		  Name:      "request_counter_to_api",
 		  Help:      "counts incoming requests to api",
-	   })
+	   }),
+	
+	emp_get_fail_counter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "golang",
+		Name:      "get_employee_fail_ctr",
+		Help:      "counts failure to fetch employee",
+	}),
+
+	emp_create_fail_counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "golang",
+			Name:      "create_employee_fail_ctr",
+			Help:      "counts failure to create employee",
+		}),
+	
+	emp_update_fail_counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "golang",
+			Name:      "update_employee_fail_ctr",
+			Help:      "counts failure to update employee",
+		}),
+	
+	emp_delete_fail_counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "golang",
+			Name:      "delete_employee_fail_ctr",
+			Help:      "counts failure to delete employee",
+		})
   )
 
 func Register() {
-	prometheus.MustRegister(counter)
+	prometheus.MustRegister(incoming_api_req_counter)
+	prometheus.MustRegister(emp_get_fail_counter)
+	prometheus.MustRegister(emp_create_fail_counter)
+	prometheus.MustRegister(emp_update_fail_counter)
+	prometheus.MustRegister(emp_delete_fail_counter)
 }
 
 func MaskPromHandler(w http.ResponseWriter, r *http.Request) {
